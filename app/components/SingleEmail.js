0841a9d98b98ede951d170f9b2f617005448059b
Vincent <vpang@law.fordham.edu>
2021-03-30 17:47:30 -0400

    Date Formatting Updated

--- app/components/SingleEmail.js
+++ app/components/SingleEmail.js
@@ -2,17 +2,14 @@ import React from 'react';
 
 class SingleEmail extends React.Component {
   render(){
-    const {email} = this.props;
+    const {email, email:{Date}} = this.props;
     return (
       <div className="email-row">
-        <div className="primary-info">
-          <p>{email.To}</p>
-          <p>{email.From}</p>
-          <p>{email.Subject}</p>
-          <p>01/10/2021</p>
-        </div>
-        <div className="secondary-info">
-
+        <div className="email-info">
+          <p className="column-to-from">{email.To}</p>
+          <p className="column-to-from">{email.From}</p>
+          <p className="column-subject subject-row">{email.Subject}</p>
+          <p className="column-date row-date">{`${Date.getFullYear()}/${(Date.getMonth()+1).toString().padStart(2, "0")}/${(Date.getDate()).toString().padStart(2, "0")}`}</p>
         </div>
       </div>
     )
