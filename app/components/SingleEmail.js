a67fe88400d7c0ffc31233f10620a772342633e0
Vincent <vpang@law.fordham.edu>
2021-03-30 18:49:15 -0400

    Date Formatter Updated

--- app/components/SingleEmail.js
+++ app/components/SingleEmail.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 class SingleEmail extends React.Component {
 
-  formatDate(Date){
-    // calculate and add additional formatting based on some criteria...
-    return `${Date.getFullYear()}/${(Date.getMonth()+1).toString().padStart(2, "0")}/${(Date.getDate()).toString().padStart(2, "0")}`
+  formatDate(emailDate){
+    const currDate = new Date();
+    const diff = (currDate.getTime() - emailDate.getTime()) / 86400000; //(1000*60*60*24)
+    console.log(diff)
+    if(diff < 1){
+      //calculate hours & minutes
+    }
+    if(currDate.getFullYear() === emailDate.getFullYear()){
+      //display only month and day if current year
+      const monthAbbreviations = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'];
+      const monthName = monthAbbreviations[emailDate.getMonth()];
+      return `${monthName} ${(emailDate.getDate()).toString().padStart(2, "0")}`
+    }
+    return `${emailDate.getFullYear()}/${(emailDate.getMonth()+1).toString().padStart(2, "0")}/${(emailDate.getDate()).toString().padStart(2, "0")}`
   }
 
   render(){
