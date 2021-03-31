90bbf3290705da671f2390da30a30d14d29ea9af
Vincent <vpang@law.fordham.edu>
2021-03-31 16:28:26 -0400

    Filter Icon Arrows added to Component

--- app/components/QueryResults.js
+++ app/components/QueryResults.js
@@ -7,10 +7,45 @@ class QueryResults extends React.Component {
     return (
       <div id="query-data-container" >
         <div id="table-header">
-          <p id="to" className="column-to-from" onClick={e => columnFilter(e)}>From</p>
-          <p id="from" className="column-to-from" onClick={e => columnFilter(e)}>To</p>
-          <p id="subject" className="column-subject" onClick={e => columnFilter(e)}>Subject</p>
-          <p id="date" className="column-date" onClick={e => columnFilter(e)}>Date</p>
+          <p
+            id="to"
+            className="column-to-from toggle-color"
+            onClick={e => columnFilter(e)}
+            style={{color: 'gray'}}
+          >From
+          <img src="assets/icon_arrow01.svg" className="filter-icons" id="to-asc" hidden={true}/>
+          <img src="assets/icon_arrow01.svg" className="filter-icons rotate-180" id="to-desc" hidden={true}/>
+          </p>
+
+          <p
+            id="from"
+            className="column-to-from toggle-color"
+            onClick={e => columnFilter(e)}
+            style={{color: 'gray'}}
+          >To
+          <img src="assets/icon_arrow01.svg" className="filter-icons" id="from-asc" hidden={true}/>
+          <img src="assets/icon_arrow01.svg" className="filter-icons rotate-180" id="from-desc" hidden={true}/>
+          </p>
+
+          <p
+            id="subject"
+            className="column-subject toggle-color"
+            onClick={e => columnFilter(e)}
+            style={{color: 'gray'}}
+          >Subject
+          <img src="assets/icon_arrow01.svg" className="filter-icons" id="subject-asc" hidden={true}/>
+          <img src="assets/icon_arrow01.svg" className="filter-icons rotate-180" id="subject-desc" hidden={true}/>
+          </p>
+
+          <p
+            id="date"
+            className="column-date toggle-color"
+            onClick={e => columnFilter(e)}
+            style={{color: 'black'}}
+          >Date
+            <img src="assets/icon_arrow01.svg" className="filter-icons" id="date-asc"/>
+            <img src="assets/icon_arrow01.svg" className="filter-icons rotate-180" id="date-desc" hidden={true}/>
+          </p>
         </div>
         {queryData.map(
           (singleRecord, idx) => { return <SingleEmail key={idx} email={singleRecord} />}
